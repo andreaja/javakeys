ca7a71997782bcd2a43558b96f6ce0c2dd2715df
Andreas Jacobsen <andreaja@ifi.uio.no>
2010-04-11 21:48:58 +0200

    More java keyevent and keystroke information.

--- KeyDebugWindow.java
+++ KeyDebugWindow.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
 
 /**
  * From IDEA issue tracker IDEABKL-3826
@@ -11,10 +12,22 @@ class KeyDebugWindow extends JFrame {
     KeyDebugWindow() {
         addKeyListener(new KeyAdapter() {
                 public void keyPressed(KeyEvent event) {
+                    System.out.println(event);
                     System.out.println("keyPressed called; " +
                                        "code: " + event.getKeyCode() +
                                        ", char: " + event.getKeyChar() +
-                                       ", location: " + event.getKeyLocation());
+                                       ", location: " + event.getKeyLocation() +
+                                       ", keytext: " + KeyEvent.getKeyText(event.getKeyCode())
+                                       );
+                    KeyStroke stroke = KeyStroke.getKeyStrokeForEvent(event);
+                    System.out.println("Stroke: " +stroke+
+                                       " code: " +stroke.getKeyCode() +
+                                       " char: " + stroke.getKeyChar());
+                    KeyStroke strokeChar = KeyStroke.getKeyStroke(event.getKeyChar());
+                    System.out.println("StrokeChar: " +strokeChar+
+                                       " code: " +strokeChar.getKeyCode() +
+                                       " char: " + strokeChar.getKeyChar());
+
                 }
             });
     }
